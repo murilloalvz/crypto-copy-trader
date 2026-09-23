36fe95914c88d4725f145445c1d4c5db5dc66204
Murillo Lourenço <murilloalveslou@outlook.com>
2026-09-23 10:27:40 -0300

    test: freeze V7 live shadow contract

--- tests/test_rust_signal_plane_live_shadow_v2.py
+++ tests/test_rust_signal_plane_live_shadow_v2.py
@@ -22,10 +22,10 @@ from benchmarks.integrated_market_signal_plane_v1.live_shadow import (
 
 
 class RustSignalPlaneLiveShadowV2Tests(unittest.TestCase):
-    def test_v5_signal_batch_contract_is_frozen(self):
+    def test_v7_rust_hotpath_contract_is_frozen(self):
         self.assertEqual(
             VERSION,
-            "rust_signal_plane_live_shadow_v6_target_prefilter",
+            "rust_signal_plane_live_shadow_v7_rust_hotpath",
         )
         self.assertEqual(INGRESS_QUEUE_SIZE, 8192)
         self.assertEqual(SURFACE_IDLE_TIMEOUT_SECONDS, 30.0)
